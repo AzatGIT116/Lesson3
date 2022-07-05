0c3b55dbcdbb96185b93277b7dfcfa5606809e7f
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-05 21:55:19 +0300

    add Zadacha 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 10000 && number <= 99999)
+{
+    string str= number.ToString();
+    if (str[0]==str[4]&& str[1]==str[3])
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else 
+{
+    Console.WriteLine("Не пятизначное число");
+}
